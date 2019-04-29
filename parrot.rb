20f1d72f098ee3c5eac73c2e930d467f69be21d9
aeisha simmons <ashicks2017@outlook.com>
2019-04-29 05:59:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def #parrot ()
+def #parrot(talk = "Squawk")
+
+  puts "This parror know how to talk, and say #{talk} /n."
+ talk
+ 
+end
+
+#parrot
